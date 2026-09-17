d6a01b77666f96ee5094fa06be30616e39a7a784
collinlarson11 <collinlarson11@gmail.com>
2026-09-17 22:25:22 +0000

    Work in Progress

---
+++ CleanArchitectureDemo.Domain/ITodoItem.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchitectureDemo.Domain;
+
+public interface ITodoItem
+{
+    public string Titel { get; set; }
+    public bool IsCompleted { get; set; }
+}
---
+++ CleanArchitectureDemo.Domain/ITodoRepository.cs
@@ -0,0 +1,14 @@
+namespace CleanArchitectureDemo.Domain;
+
+public interface ITodoRepository
+{
+    public void CreateToDoItem(string title, bool IsCompleted = false);
+
+    // TODO: Create a Repository Read Method
+
+    // TODO: Create a Repository Update Method
+
+    // TODO: Create a Repository Delete Method
+
+    public IEnumerable<TodoItem> ListTodos();
+}
