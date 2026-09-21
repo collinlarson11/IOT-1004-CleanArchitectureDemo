09aa9d568ae3b00a5b477343436cbb197805076e
collinlarson11 <collinlarson11@gmail.com>
2026-09-21 21:00:12 +0000

    Now working after following allong with my professor's instructions. Think it's getting fixed.

---
+++ CleanArchitectureDemo.UserInterface/Program.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitectureDemo.Application;
+using CleanArchitectureDemo.Domain;
+
+Console.WriteLine("Hello, World!");
+
+var todo = new TodoItem("Hello");
+
+Console.WriteLine(todo.Title);
+
+var repo = new TodoRepository();
+
+repo.CreateToDoItem("Hello 2");
+
+var todoList = repo.ListTodos();
+
+Console.WriteLine(todoList.First().Title);
